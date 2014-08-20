5525d918ac2efb75df87fb5fe64b9ecae5c59eb3
Denhart <Denhart@gmail.com>
2014-08-20 14:01:28 +0200

    Small php changes

--- Server files/denup.php
+++ Server files/denup.php
@@ -5,17 +5,12 @@ if($secret == $secretkey){
   $data = file_get_contents('php://input');
   if ($data) {
     $filename = date('Y_m_d-H-i-s');
-    //Has to be checked due to legacy from old code. 
-    if ($_GET[type]) {
-      $dataFile = "$path" . "$filename" . ".$_GET[type]";
-    } else{
-      $dataFile = "$path" . "$filename" . '.jpg';
-    }
+    $dataFile = "$path" . "$filename" . ".$_GET[type]";
     $fh = fopen($dataFile, 'w') or die();
     $data = base64_decode($data);
     fwrite($fh, $data);
     fclose($fh);
-    echo "$host" . "$filename".'.jpg';
+    echo "$host" . "$filename".".$_GET[type]";
     return;
   }
 }else{
